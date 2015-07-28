09420de54b7556fb5058d8a181b37ba89679b3d0
Sabarish <sbv1g13@soton.ac.uk>
2015-07-28 10:13:01 +0100

    Added more functionality, saved from ipython notebook to module 'flowField.py'. Contains methods: copyArray, real, imag, ddt, ddx, ddz, ddy, slice, getScalar, appendField, verify, view1d,view4d

---
+++ flowField.py
@@ -0,0 +1,322 @@
+import numpy as np
+import scipy as sp
+#from scipy.linalg import norm
+from warnings import warn
+from pseudo import chebdif
+#from pseudo.py import chebint
+
+defaultDict = {'alpha':1.14, 'beta' : 2.5, 'omega':0.0, 'L': 23, 'M': 23, 'nd':4,'N': 35, 'K':0,
+               'ReLam': 400.0, 'isPois':0.0, 'noise':0.0 }
+
+def verify_dict(tempDict):
+    '''Verify that the supplied flowDict has all the parameters required'''
+    change_parameters = False
+    if tempDict is None:
+        tempDict = defaultDict
+        warn('No flowDict was supplied. Assigning the default dictionary')
+    else: 
+        for key in defaultDict:
+            if key not in tempDict:
+                change_parameters = True
+                tempDict[key] = defaultDict[key]
+    [tempDict['K'],tempDict['L'],tempDict['N'],tempDict['isPois']] = [int(abs(k)) for k in [tempDict['K'],tempDict['L'],tempDict['N'],tempDict['isPois']]]
+    tempDict['M'] = int(tempDict['M'])
+    if change_parameters:
+        warn('The supplied dictionary had some parameters missing. These were provided from the default dictionary')
+    return tempDict
+
+def read_dictFile(dictFile):
+    '''Read flowDict from file. MUST use "flowConfig.txt" as template. '''
+    tempDict = {}
+    with open("flowConfig.txt",'r') as f:
+        for line in f:
+            (key,val) = line.split()[:2]
+            tempDict[key] = float(val)    
+    return tempDict
+
+def makeVector(*args):
+    '''Concatenate flowField objects. Use this to create a vector flowField from a scalar flowField as
+    uvw = makeVector(u,v,w)'''
+    ff = args[0]
+    if not isinstance(ff,flowField):
+        raise RuntimeError('makeVector takes as arguments only instances of flowField class')
+        return
+    for v in args[1:]:
+        if not isinstance(v,flowField):
+            raise RuntimeError('makeVector takes as arguments only instances of flowField class')
+        ff = ff.appendField(v)
+    return ff
+    
+
+class flowField(np.ndarray):
+    ''' Provides a class to define u,v,w,p in 4D: time, x,z,y. 
+    Ordered as (omega,alpha,beta,nd,y): omega, alpha, beta are Fourier modes in t,x,z respectively.
+    nd is an index going from 0 to 3 for u,v,w,p. 
+    y is the array of Chebyshev collocation nodes
+    The dictionary is fundamental to the workings of the flowField class. 
+        All three arguments can be used to provide a dictionary (arr can be an instance of flowField).
+        flowDict argument has highest priority in defining the dictionary, 
+            followed by dictFile
+            followed by arr.flowDict
+        If none of the above arguments provide a flowDict, a default dictionary (defined in the module) is used.
+        A warning message is printed when the default dictionary is used.
+
+    Methods: 
+        slice(K,L,M,nd,N): Make grid finer or coarser along any direction
+        view1d(): Return 1-d array of class flowField
+        view4d(): Return 4-d array of class flowField
+        etc... 
+        Create an object using defaults as ff = flowField() and use tab completion to see all the methods'''
+    
+    def __new__(cls, arr=None, flowDict=None, dictFile= None):
+        '''Creates a new instance of flowField class with arguments (arr=None,flowDict=None,dictFile=None)
+        '''
+        if flowDict is None:
+            if dictFile is None:
+                flowDict = defaultDict
+                if hasattr(arr,'flowDict'):
+                    flowDict = arr.flowDict
+            else:
+                flowDict = verify_dict(read_dictFile(dictFile))
+        else:
+            flowDict = verify_dict(flowDict)
+        
+        
+        L = flowDict['L']
+        M = flowDict['M']
+        N = flowDict['N']
+        K = flowDict['K']
+        nd = flowDict['nd']
+        nt = 2*K+1
+        nx = 2*L+1
+        nz = int(3.*abs(M)/2. - M/2. + 1)     # = 1 if M=0;    = M+1 if M>0;    = 2*|M|+1 if M<0
+        
+        if arr is None:
+            #obj =  np.zeros((nt,nx,nz,nd,N),dtype=np.complex).view(cls)
+            obj = np.ndarray.__new__(flowField,shape=(nt,nx,nz,nd,N),dtype=np.complex,buffer=np.zeros(nt*nx*nz*nd*N,dtype=np.complex))
+        else:
+            if arr.dtype == np.float:
+                arr = (arr+1.j*np.zeros(arr.shape))
+            obj = np.ndarray.__new__(flowField,shape=(nt,nx,nz,nd,N),dtype=np.complex,buffer=arr)
+        
+        #print(norm(obj))
+        
+        if obj.size != (nx*nz*nt*nd*N):
+            raise RuntimeError('The parameters in the dictionary are not consistent with the size of the supplied array')
+        
+        obj.flowDict = flowDict
+        obj.nx = nx
+        obj.nz = nz
+        obj.nt = nt
+        obj.N = N
+        obj.nd = flowDict['nd']
+        return obj
+        
+    
+    def __array_finalize__(self,obj):
+        if self.dtype != np.complex:
+            warn('flowField class is designed to work with complex array entries\n'+
+                 'To obtain real/imaginary parts of an instance, use class methods "real()" and "imag()"')
+        if isinstance(obj, flowField):
+            self.flowDict = getattr(self,'flowDict',obj.flowDict.copy())
+            self.nt = getattr(self,'nt',obj.nt)
+            self.nx = getattr(self,'nx',obj.nx)
+            self.nz = getattr(self,'nz',obj.nz)
+            self.nd = getattr(self,'nd',obj.nd)
+            self.N = getattr(self,'N',obj.N)
+            return
+        elif obj != None:
+            raise RuntimeError('View-casting np.ndarray is not supported since dictionaries cannot be passed. \n'+
+                               'To initialize class instance from np.ndarray, use constructor call:flowField(arr=myArray,dictFile=myFile)')
+        return
+
+    
+    def verify(self):
+        '''Ensures that the size of the class array is consistent with the dictionary entries. 
+        Use this when writing new methods or tests'''
+        self.flowDict = verify_dict(self.flowDict)
+        if not ((self.nt == 2*self.flowDict['K']+1) and (self.nx == 2*self.flowDict['L']+1) and 
+                (self.nz == int(3.*abs(self.flowDict['M'])/2. - self.flowDict['M']/2. + 1)) and
+                (self.N == self.flowDict['N']) and (self.nd == self.flowDict['nd'])): 
+            raise RuntimeError('The shape attributes of the flowField instance are not consistent with dictionary entries')
+        if not (self.size == self.nt*self.nx*self.nz*self.nd*self.N):
+            raise RuntimeError('The size of the flowField array is not consistent with its shape attributes')
+        
+
+    def view1d(self):
+        ''' Returns a 1d view. 
+        Don't try to figure out what the ordering is, just use self.view4d() to get an organized view'''
+        return self.reshape(self.size)
+    
+    def view4d(self):
+        ''' Returns a 4d view (actually, a 5-D array): (omega, alpha, beta, field=u,v,w,p, N)'''
+        return self.reshape((self.nt,self.nx,self.nz,self.nd,self.N))
+
+    def slice(self,K=None,L=None,M=None,nd=None,N=None):
+        '''
+        Returns a class instance with increased/reduced K,L,M,nd,N
+        Call as new_inst = myFlowField.slice(K=Knew,L=Lnew,N=Nnew)) to change values of K,L,N without affecting M (and nd)
+        When the number of Fourier modes (K,L,M, or nt,nx,nz) are smaller than what is requested, 
+            additional zero modes are added. For Chebyshev nodes, interpolation is used'''
+        obj = self.copyArray()
+        nxt = self.nx
+        ntt = self.nt
+        nzt = self.nz
+        ndt = self.nd
+        Nt = self.N
+        flowDict_temp = self.flowDict.copy()
+        if K is not None:
+            K = int(abs(K))
+            Kt = flowDict_temp['K']               # Temporary name for 'K' of self
+            if K <= Kt:
+                obj = obj[Kt-K:Kt+K+1]
+            else: 
+                obj = np.concatenate((  np.zeros((Kt-K,nxt,nzt,ndt,Nt),dtype=np.complex), obj,
+                               np.zeros((Kt-K,nxt,nzt,ndt,Nt),dtype=np.complex)  ), axis=0)
+            flowDict_temp['K']= K
+            ntt = 2*K+1
+        
+        if L is not None:
+            L = int(abs(L))
+            Lt = flowDict_temp['L']               # Temporary name for 'L' of self
+            if L <= Lt:
+                obj = obj[:,Lt-L:Lt+L+1]
+            else: 
+                obj = np.concatenate((  np.zeros((ntt,abs(Lt-L),nzt,ndt,Nt),dtype=np.complex), obj,
+                               np.zeros((ntt,abs(Lt-L),nzt,ndt,Nt),dtype=np.complex)  ), axis=1)
+            flowDict_temp['L']= L
+            nxt = 2*L+1
+        
+        if M is not None:
+            M = int(M)
+            Mt = flowDict_temp['M']               # Temporary name for 'M' of self
+            nzt = int(3.*abs(M)/2. - M/2. + 1)     # = 1 if L=0;    = L+1 if L>0;    = 2*|L|+1 if L<0
+            
+            if M*Mt >=0: 
+                if abs(M) <= abs(Mt): # Case 1.A: Truncate
+                    nz0 = int((abs(Mt)-Mt)/2)     # = Mt for Mt< 0, = 0 otherwise
+                    nzm1 = nz0 - int((abs(M)-M)/2) 
+                    nzp1 = nz0 + abs(M) + 1
+                    obj = obj[:,:,nzm1:nzp1]
+                else:  # Case 1.B: Extend using zero modes
+                    nzplus = int(abs(M)-abs(Mt))
+                    if M<0: 
+                        obj = np.concatenate(( np.zeros((ntt,nxt,abs(Mt-M),ndt,Nt),dtype=np.complex), obj,
+                               np.zeros((ntt,nxt,abs(Mt-M),ndt,Nt),dtype=np.complex)  ), axis=2)
+                    else:
+                        obj = np.concatenate(( obj,
+                               np.zeros((ntt,nxt,abs(Mt-M),ndt,Nt),dtype=np.complex) ), axis=2)
+            elif M > 0:          # Case 2: Get only modes [0,b,..,|M|b] from [-|Mt|*b,..,0,b,..,|Mt|*b]
+                if abs(M) <= abs(Mt): # Case 2.A: |M|< |Mt|, so truncate
+                    nz0 = int((abs(Mt)-Mt)/2)
+                    nzp1 = nz0 + M + 1
+                    obj = obj[:,:,nx0:nzp1]
+                else:    # Case 2.B: |M| > |Mt|, so add zero modes 
+                    obj = np.concatenate(( obj[:,:,abs(Mt):], 
+                               np.zeros((ntt,nxt,abs(Mt-M),ndt,Nt),dtype=np.complex) ), axis=2)
+            else: # Case 3: Get modes [-|M|b,...,0,b,..,|M|b], given [0,b,..,|Mt|b]
+                if abs(M) <= abs(Mt):        # Case 3.A: Truncate on positive, extend with conjugates on negative
+                    obj = np.concatenate(( obj[::-1,::-1,abs(M):0:-1].conjugate(), obj[:,:,:abs(M)+1] ), axis=2)
+                else:            # Case 3.B: Extend on positive with zeros, extend on negative with conjugates and zeros
+                    # Doing the extension with conjugates on negative first:
+                    obj = np.concatenate(( obj[::-1,::-1,:0:-1].conjugate(), obj ), axis=2)
+                    # Adding zeros on positive and negative:
+                    obj = np.concatenate((  np.zeros((ntt,nxt,abs(Mt-M),ndt,Nt),dtype=np.complex), obj,
+                               np.zeros((ntt,nxt,abs(Mt-M),ndt,Nt),dtype=np.complex) ), axis=2)
+            flowDict_temp['M']= M
+        
+        if N is not None:
+            N = abs(int(N))
+            Nt = flowDict_temp['N']
+            if N != Nt:
+                y = chebdif(Nt,1)[0]
+                obj_t = obj.reshape((obj.size/Nt,Nt))
+                obj = np.zeros((obj_t.size/Nt,N),dtype=np.complex)
+                for n in range(obj_t.size/N):
+                    obj[n] = chebint(obj_t[n],y)
+            obj = obj.reshape(obj.size)
+            flowDict_temp['N'] = N
+        
+        obj = flowField(arr=obj, flowDict = flowDict_temp).view4d()
+        
+        if nd is not None:
+            nd = np.asarray([nd])
+            nd = nd.reshape(nd.size)
+            obj = obj[:,:,:,nd]
+            obj.flowDict['nd'] = nd.size
+            obj.nd = nd.size
+        
+        obj.verify()
+        return obj
+    
+    def getScalar(self,nd=0):
+        '''Returns the field Variable in the flowField instance identified by the argument "nd".
+        Default for "nd" is 0, the first scalar in the flowField (u)'''
+        if type(nd) != int:
+            raise RuntimeError('getScalar(nd=0) only accepts integer arguments')
+        obj = self.view4d()[:,:,:,nd].copy()
+        obj.flowDict['nd'] = 1
+        obj.nd = 1
+        return obj.view4d()
+
+    def appendField(self,obj):
+        '''Append a field at the end of "self". To append "p" to "uVec", call as uVec.appendField(p)
+        Note: Both uVec and p must be flowField objects, each with their flowDict'''
+        if not isinstance(obj,flowField):
+            raise RuntimeError('Only flowField objects can be appended to a flowField object')
+        tempDict = self.flowDict.copy()
+        tempDict['nd'] += obj.flowDict['nd']
+        v1 = self.view4d().copyArray()
+        v2 = obj.view4d().copyArray()
+        return flowField(arr=np.append(v1,v2,axis=3), flowDict=tempDict)
+    
+    def copyArray(self):
+        ''' Returns a copy of the np.ndarray of the instance. 
+        This is useful for manipulating the entries of a flowField without bothering with all the checks'''
+        return self.view(np.ndarray).copy()
+    
+    def real(self):
+        ''' Returns the real part of the flowField (the entries are still complex, with zero imaginary parts)'''
+        return flowField(arr=self.copyArray().real,flowDict=self.flowDict)
+    
+    def imag(self):
+        ''' Returns the imaginary part of the flowField (the entries are still complex, with zero imaginary parts)'''
+        return flowField(arr=self.copyArray().imag,flowDict=self.flowDict)
+    
+    def ddt(self):
+        ''' Returns a flowField instance that gives the partial derivative along "t" '''
+        if self.nt == 1:
+            return 1.j*self.flowDict['omega']*self.copy()
+        partialT = self.view4d().copy()
+        kArr = np.arange(-self.flowDict['K'],self.flowDict['K']+1).reshape(self.nt,1,1,1,1)
+        return partialT
+    
+    def ddx(self):
+        ''' Returns a flowField instance that gives the partial derivative along "x" '''
+        if self.nx == 1:
+            return 1.j*self.flowDict['alpha']*self.copy()
+        partialX = self.view4d().copy()
+        lArr = np.arange(-self.flowDict['L'],self.flowDict['L']+1)
+        tempArr = (np.ones((self.nt,self.nx))*lArr).reshape(self.nt,self.nx,1,1,1)
+        partialX[:] = 1.j*self.flowDict['alpha']*tempArr*partialX
+        return partialX
+            
+    def ddz(self):
+        ''' Returns a flowField instance that gives the partial derivative along "z" '''
+        if self.nz == 1:
+            return 1.j*self.flowDict['beta']*self.copy()
+        partialZ = self.view4d().copy()
+        mArr = np.arange((self.flowDict['M']-np.abs(self.flowDict['M']))/2,self.flowDict['M']+1)
+        tempArr = (np.ones((self.nt,self.nx,self.nz))*mArr).reshape(self.nt,self.nx,self.nz,1,1)
+        partialZ[:] = 1.j*self.flowDict['beta']*tempArr*partialZ
+        return partialZ
+        
+    def ddy(self):
+        ''' Returns a flowField instance that gives the partial derivative along "y" '''
+        partialY = self.view1d().copy()
+        N = partialY.flowDict['N']
+        D = (chebdif(N,1)[1]).reshape(N,N)
+        for n in range(self.nt*self.nx*self.nz*self.nd):
+            partialY[n*N:(n+1)*N] = np.dot(D, partialY[n*N:(n+1)*N])
+        return partialY.view4d()
+        
---
+++ pseudo.py
@@ -0,0 +1,318 @@
+# pseudo.py
+#
+# Contains functions:
+#		y,DM = chebdif(N,M): Takes number of nodes 'N', and required number of derivatives'M'
+#					and returns Chebyshev nodes 'y', and differentiation matrices in 'DM'
+#		DM = poldif(x,M): Takes a general grid 'x', and required number of derivatives'M'
+#					and returns differentiation matrices in 'DM' based on Lagrange interpolation
+#		w = clencurt(N): Takes number of nodes 'N'
+#					and returns Clenshaw-Curtis weights 'w', for 'N' Chebyshev nodes between -1 and 1
+##--------------------------------------------------------------------------------------------------------
+# Update:
+# Earlier version used 2-D arrays, as used by Matlab, as the default. Vectors
+#  were stored as (N,1) arrays. 
+# This version uses 1-D arrays as the default for vectors
+
+
+## ----------------------------------------------------
+## ACKNOWLEDGEMENT
+
+# Implementation for chebdif and poldif is based on Weideman and Reddy's differentiation suite for Matlab.
+# Most of the code is A line-by-line translation of their functions.
+# This translation is done, in spite of available pseudo-spectral differentiation codes since the Matlab
+# suite incorporates features that improve accuracy, which becomes important for calculating higher derivatives.
+# Notably, the chebdif function uses trigonometric identities in [0,pi/2] to calculate x(k)-x(j), and higher 
+# derivatives are explicitly calculated, instead of using D*D*..
+
+# The webpage for the differentiation suite:
+# http://dip.sun.ac.za/~weideman/research/differ.html
+
+
+# 'clencurt' is from a blog by Dr. Greg von Winckel:
+#	http://www.scientificpython.net/pyblog/clenshaw-curtis-quadrature	
+# However, a minor change has been made- only the weight matrix is returned, as opposed to returning both the weights
+#		and the nodes, as in the origion version on the blog
+
+
+
+###--------------------------------------------------------
+
+import scipy as sp 
+from operator import mul
+from scipy.linalg import toeplitz
+from scipy.fftpack import ifft
+
+def chebdif(N,M):
+
+	I = sp.identity(N)		# Identity matrix
+	#L = bool(I)
+
+	n1 = sp.floor(N/2.)		# Indices for flipping trick
+	n2 = sp.ceil(N/2.)
+
+	k=sp.vstack(sp.linspace(0.,N-1.,N))	# Theta vector
+	th=k*sp.pi/(N-1.)
+
+	x=sp.sin(sp.pi*sp.vstack(sp.linspace(N-1.,1.-N,N))/2./(N-1.))	# Chebyshev nodes
+
+	T = sp.tile(th/2.,(1,N))
+	DX= 2.*sp.sin(T.T+T)*sp.sin(T.T-T)	# Compute dx using trigonometric identity, improves accuracy
+
+	DX= sp.vstack((DX[0:n1,:], sp.flipud(sp.fliplr([-1.*el for el in DX[0:n2,:]]))))
+	DX = DX+I			# Replace 0s in diagonal by 1s (required for calculating 1/dx)
+
+	C= toeplitz(pow(-1,k))			# Matrix with entries c(k)/c(j)
+	C[0,:] = [2.*el for el in C[0,:]]	
+	C[N-1,:] = [2.*el for el in C[N-1,:]]
+	C[:,0] = [el/2. for el in C[:,0]]
+	C[:,N-1] = [el/2. for el in C[:,N-1]]
+
+
+	Z = [1./el for el in DX]		# Z contains 1/dx, with zeros on diagonal
+	Z = Z - sp.diag(sp.diag(Z))
+
+
+	D = sp.identity(N)
+	DM = sp.zeros((N,N,M))			# Output matrix, contains 'M' derivatives
+
+	for ell in range(0,M):
+		D[:,:] = Z*(C*(sp.tile(sp.diag(D),(N,1)).T)-D)	
+		D[:,:] = [(ell+1)*l for l in D]			# Off-diagonal elements
+		trc = sp.array([-1.*l for l in sp.sum(D.T,0)])
+		D = D - sp.diag(sp.diag(D)) + sp.diag(trc)	# Correcting the main diagonal
+		DM[:,:,ell] = D
+
+   # Return collocation nodes as a 1-D array
+	return  x[:,0] , DM
+
+
+def poldif(x,M):
+	N= sp.size(x)
+	x = sp.vstack(sp.array(x))
+	a,b = x.shape
+	if b != 1:
+		print ('x is not a vector')
+
+	alpha = sp.ones((N,1))
+	B = sp.zeros((M,N))
+
+	I = sp.identity(N)
+
+	XX = sp.tile(x,(1,N))
+	DX=XX-XX.T
+	DX = DX + I
+
+	c = sp.ones((N,1))
+	sp.prod(DX,axis=1,out=c)
+
+	C = sp.tile(c, (1,N))
+	C = C/C.T
+
+	Z = sp.array([1./l for l in DX])
+	Z = Z - sp.diag(sp.diag(Z))
+
+	X = Z.T
+
+	X = sp.delete(X.T,sp.linspace(0,N*N-1,N))
+	X = (X.reshape((N,N-1))).T
+
+	Y = sp.ones((N-1,N))
+	D = sp.identity(N)
+	DM = sp.ones((N,N,M))
+
+	for ell in range(0,M):
+		Y = sp.cumsum(sp.vstack((B[ell,:], (ell+1.)*Y[0:N-1,:]*X)) , axis=0,dtype=float)
+		D = (ell+1.)*Z*(C*(sp.tile(sp.diag(D),(N,1)).T)-D)
+		D = D - sp.diag(sp.diag(D)) + sp.diag(Y[N-1,:])
+		DM[:,:,ell] = D
+	
+	return DM
+	
+	
+	
+def clencurt(n1):
+  """ Computes the Clenshaw Curtis weights """
+  if n1 == 1:
+    x = 0
+    w = 2
+  else:
+    n = n1 - 1
+    C = sp.zeros((n1,2))
+    k = 2*(1+sp.arange(sp.floor(n/2)))
+    C[::2,0] = 2/sp.hstack((1, 1-k*k))
+    C[1,1] = -n
+    V = sp.vstack((C,sp.flipud(C[1:n,:])))
+    F = sp.real(ifft(V, n=None, axis=0))
+    x = F[0:n1,1]
+    w = sp.hstack((F[0,0],2*F[1:n,0],F[n,0]))
+  return w
+
+
+def chebnorm(vec,N):
+	vect = sp.asarray(vec)
+	vect = vect.reshape((vect.size,1))
+	wvec = clencurt(N)
+	wvec = wvec.reshape((1,N))
+	
+	repN = vect.size/N
+	
+	return sp.sqrt(  abs(sp.dot( sp.tile(wvec,(1,repN)), vect.conjugate()*vect ))[0,0]   )
+
+# Clencurt-weighted norm on internal Chebyshev collocation nodes- i.e., excluding 1,-1
+def chebnorm_int(vec,N):
+   m = vec.size/(N-2)
+   vect = sp.asarray(vec).reshape((m,N-2))
+   wvec = clencurt(N)
+   wvec = wvec[1:N-1]
+
+   return sp.sqrt( abs(  sp.sum(sp.dot( vect*vect.conjugate(),wvec )) ))
+
+def chebdotvec(vec1,vec2,N):
+    # This function computes inner products of vectors ONLY.
+    if (vec1.ndim != 1) or (vec2.ndim != 1) or (not isinstance(N,int)):
+        raise RuntimeError("chebdotvec only accepts vector arguments, and integer 'N'")
+
+    if (vec1.size % N != 0) or (vec2.size % N != 0) or (vec1.size != vec2.size):
+        raise RuntimeError("Vector sizes not consistent with each other or with 'N'")
+
+    wvec = clencurt(N)
+    res = 0.
+    wvec = sp.tile(wvec,(vec1.size/N,))
+
+    return sp.dot(wvec, vec1.conjugate()*vec2)
+
+def chebdotvec_int(vec1,vec2,N):
+    m = vec1.size/(N-2)
+    wvec = clencurt(N)
+    wvec = wvec[1:N-1]
+    vec1t = vec1.reshape((m,N-2))
+    vec2t = vec2.reshape((m,N-2))
+
+    return sp.sum( sp.dot( vec1t*vec2t.conjugate(), wvec ))
+
+def chebdot(arr1,arr2,N):
+    # The arguments arr1 and arr2 can either be vectors or matrices
+    if (arr1.ndim ==1) and (arr2.ndim == 1):
+        return chebdotvec(arr1,arr2,N)
+
+
+    if (arr1.ndim == 2) and (arr2.ndim == 1):
+        l = arr1.shape[0]
+        dot_prod = sp.zeros(l)
+        for k in range(0,l):
+            dot_prod[k] = chebdotvec(arr1[k],arr2,N)
+
+    elif (arr1.ndim == 1) and (arr2.ndim == 2):
+        l = arr2.shape[0]
+        dot_prod = sp.zeros(l)
+        for k in range(0,l):
+            dot_prod[k] = chebdotvec(arr1,arr2[k],N)
+
+    elif (arr1.ndim == 2) and (arr2.ndim ==2):
+        l1 = arr1.shape[0]
+        l2 = arr2.shape[0]
+        dot_prod = sp.zeros((l1,l2))
+        for k1 in range(0,l1):
+            for k2 in range(0,l2):
+                dot_prod[k1,k2] = chebdotvec(arr1[k1],arr2[k2],N)
+
+    else:
+        raise RuntimeError("Input arguments are neither vectors nor 2D matrices")
+
+    return dot_prod
+
+def chebnorm1(vec,N):
+	vect = sp.asarray(vec)
+	vect = vect.reshape((vect.size,1))
+	wvec = clencurt(N)
+	wvec = wvec.reshape((1,N))
+	
+	repN = vect.size/N
+	
+	return abs(sp.dot( sp.tile(wvec,(1,repN)), abs(vect) ))[0,0]   
+
+def chebnorm1_int(vec,N):
+   m = vec.size/(N-2)
+   vect = sp.asarray(vec).reshape((m,N-2))
+   wvec = clencurt(N)
+   wvec = wvec[1:N-1]
+
+   return abs(  sp.sum(sp.dot( abs(vect),wvec )) )   
+
+def chebnorm2(vec,N):
+	return chebnorm(vec,N)
+
+def chebnorm2_int(vec,N):
+    return chebnorm_int(vec,N)
+# Given function values at Cheb collocation nodes, returns the
+# coefficients of Chebyshev polynomials of the 1st kind
+def chebcoeffs(f):
+	if f.ndim != 1:
+		raise RuntimeError("Input is not a 1-D array")
+		
+	N = f.size
+	a = sp.fft(sp.append(f,f[N-2:0:-1]))
+	 
+	a = a[:N]/(N-1.)*sp.concatenate(([0.5],sp.ones(N-2),[0.5]))
+	
+	return a
+
+
+
+def chebcoll_vec(a):
+	if a.ndim !=1:
+		raise RuntimeError("Input is not a 1-D array")
+	
+	N = a.size
+	a = a*(N-1.)/sp.concatenate(([0.5],sp.ones(N-2),[0.5]))
+
+	f = sp.ifft(sp.append(a,a[N-2:0:-1]))
+	
+	return f[:N]
+	
+	
+
+
+def presdif(N):
+	x = chebdif(N,1)[0]
+	D1 = poldif(x[1:N-1],1)
+	D1=D1[:,:,0]
+	
+	return D1
+
+
+
+def chebdifBL(N,Y):
+	eta,DM = chebdif(2*N,2)
+	x = -Y*sp.log(eta[:N])
+	
+	D1 = (-1./Y)*eta[:N].reshape((N,1))*DM[0:N,0:N,0]
+	D2 = (1./Y/Y)*(eta[:N].reshape((N,1))*DM[0:N,0:N,0] + (eta[0:N].reshape((N,1))**2)*DM[0:N,0:N,1])
+
+	return x,D1,D2
+
+
+def presdifBL(N,Y):
+	eta,DM = chebdif(2*N,2)
+	Dpeta = poldif(eta[1:2*N-1],1)
+	
+	Dp = (-1./Y)*eta[1:N]*Dpeta[0:N-1,0:N-1,0]
+	
+	return Dp
+
+
+import numpy as np
+def chebint (fk, x):
+    speps = sp.finfo(float).eps # this is the machine epsilon
+    N = sp.size(fk)
+    M = sp.size(x)
+    xk = sp.sin(sp.pi*sp.arange(N-1,1-N-1,-2)/(2*(N-1)))
+    w = sp.ones (N)*(-1)**(sp.arange(0,N))
+    w[0] = w[0]/2
+    w[N-1] = w[N-1]/2
+    D = sp.transpose(sp.tile(x,(N,1)))-sp.tile(xk,(M,1))
+    D = 1/(D+speps*(D==0))
+    p = sp.dot(D,(w*fk))/(sp.dot(D,w))
+    return p
+
+
